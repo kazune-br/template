94d2c2816cbd1bf213b3e581ca8ece8855a74147
kazune-br <kazune.miyag92@gmail.com>
2019-11-14 05:33:40 +0900

    create template for swiching user login status

--- authentication/nuxt/layouts/default.vue
+++ authentication/nuxt/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-app-bar app>
+    <v-app-bar app height="48">
       <v-app-bar-nav-icon />
       <v-btn icon>
         <v-icon>search</v-icon>
@@ -10,6 +10,16 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-toolbar-items>
+        <template v-if="$auth.loggedIn">
+          <v-btn text> {{ $auth.user }} </v-btn>
+          <v-btn text>Logout</v-btn>
+        </template>
+        <template v-else>
+          <v-btn text to="login">Login</v-btn>
+          <v-btn text to="register">Register</v-btn>
+        </template>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
