8739adef30b0c470ebd3da99fa59ab22c65337ee
kazune-br <kazune.miyag92@gmail.com>
2019-11-13 22:52:26 +0900

    create front end by nuxt

---
+++ authentication/nuxt/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-app-bar app>
+      <v-app-bar-nav-icon />
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>apps</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ authentication/nuxt/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12">
+      <div class="text-center">
+        <logo/>
+      </div>
+    </v-col>
+    <v-col cols="12">
+      <v-card>
+      <v-card-title class="headline">
+        Welcome to the Vuetify + Nuxt.js template
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn
+          nuxt
+          to="/inspire"
+          icon
+        >
+          <v-icon>star</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      Logo,
+    }
+  }
+</script>
